987178e41fabbdbac30809e743c2ea5258e7b36a
goldreve-boop <goldreve@gmail.com>
2025-12-05 15:28:01 +0900

    Update customer.py

--- backend/routers/customer.py
+++ backend/routers/customer.py
@@ -1,1 +1,10 @@
-~�&}�-j����+�
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/api/customer",
+    tags=["Customer"]
+)
+
+@router.get("/")
+async def get_customer_root():
+    return {"message": "Customer API running"}
